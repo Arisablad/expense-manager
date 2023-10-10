d872ec7b884633065781d014136e65b62f32babb
udaceue <rjemik9@gmail.com>
2023-10-10 23:30:17 +0200

    changed responses for vercel deployment

--- Backend/middlewares/protectedRoute.js
+++ Backend/middlewares/protectedRoute.js
@@ -5,7 +5,7 @@ const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt; // get token
     if (!token) {
-      return res.status(401).json({ message: "Not authorized" });
+      res.status(401).json({ message: "Not authorized" });
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET); // verify token
     const user = await User.findById(decoded.userId).select("-password");
