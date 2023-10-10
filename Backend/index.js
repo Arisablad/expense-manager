611629b792061adcced7451850db9f848e4d58c0
udaceue <rjemik9@gmail.com>
2023-10-10 23:17:09 +0200

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -32,7 +32,8 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Origin",
     "https://expense-manager-five-green.vercel.app",
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
