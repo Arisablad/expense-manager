6e7e60262623e8aee596f65831e088e9e295392a
udaceue <rjemik9@gmail.com>
2023-10-10 23:22:11 +0200

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,30 +13,30 @@ dotenv.config();
 const PORT = process.env.PORT || 5003;
 const app = express();
 
-// const origin =
-//   process.env.NODE_ENV === "development"
-//     ? "http://localhost:5173"
-//     : `https://${process.env.DOMAIN_URL}`;
-//
-// // GLOBAL MIDDLEWARES
-// app.use(
-//   cors({
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//     origin,
-//   }),
-// );
+const origin =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5173"
+    : `https://${process.env.DOMAIN_URL}`;
 
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://expense-manager-five-green.vercel.app",
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// GLOBAL MIDDLEWARES
+app.use(
+  cors({
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    origin,
+  }),
+);
+
+// app.use((req, res, next) => {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://expense-manager-five-green.vercel.app",
+//   );
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
 app.use(bodyParser.json());
 app.use(cookieParser());
